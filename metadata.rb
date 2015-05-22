bcab9060de0789ca08efe6b21114f50e107c0b5a
Tom Wilson <tom@rollnorocks.com>
2015-05-22 11:00:05 -0600

    Moved dependencies from primary cookbook up to the test cookbook where really needed.

--- metadata.rb
+++ metadata.rb
@@ -7,6 +7,4 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 depends 'ruby', '~>0.9'
-depends 'mysql', '~> 6.0'
 depends 'database', '~> 4.0'
-depends 'mysql2_chef_gem', '~> 1.0'
--- test/fixtures/cookbooks/test/metadata.rb
+++ test/fixtures/cookbooks/test/metadata.rb
@@ -6,5 +6,6 @@ description      'Installs/Configures test'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends 'mysql', ">= 6.0"
 depends 's3_backup'
+depends 'mysql', '~> 6.0'
+depends 'mysql2_chef_gem', '~> 1.0'
